847c2d6a2f2984e0be2ba3b4b030d7c8602b9630
Alexis Torosina <alexistorosina1@gmail.com>
2022-09-20 23:01:19 -0400

    commit tree 2ded8f3f4bf9963699b0321304ded1fd61ef760b

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 31;
+const num2 = 2;
 
-// Write your code here
+const multiply = num1 * num2;
+
+let random = Math.floor(Math.random()* 10);
+
+const num3 = 16;
+const num4 = 4;
+
+const mod = num3 / num4;
+
+const max = Math.max(1,2,3,4,20);
